b4b675f41561244ba8ad11c157fef48fe2b9f358
Zachary-P-Newby <zpnewby@outlook.com>
2023-10-27 13:06:02 -0500

    Small progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![allow(unused)]
+use std::io;
+use std::io::{Write, BufReader, BufRead, BufWriter};
+use std::io::Error;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let mut path = String::new();
+
+    print!("Enter a file path: ");
+    io::stdin().read_line( &mut path);
+
+    println!("{}",path);
 }
